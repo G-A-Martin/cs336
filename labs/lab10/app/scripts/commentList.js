b0659a446e9287716c748f5c3776a44a5042aa66
Gavin Martin <gam6@maroon01.cs.calvin.edu>
2018-11-07 23:17:56 -0500

    added lab10

---
+++ labs/lab10/app/scripts/commentList.js
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import '../css/base.css';
+
+
+import Comment from './comment.js';
+
+
+
+// This is the comment list
+module.exports = React.createClass({
+    render: function() {
+      var commentNodes = this.props.data.map(function(comment) {
+        return (
+          <Comment author={comment.author} key={comment.id}>
+            {comment.text}
+          </Comment>
+        );
+      });
+      return (
+        <div className="commentList">
+          {commentNodes}
+        </div>
+      );
+    }
+  });
+  
